df428bb92d094b35dbd5cf98d97f16361c1523a0
Quang Lam <lamdieuquang0105@gmail.com>
2025-03-24 15:40:18 +0700

    ewewew

--- src/app/components/OrderDetail.tsx
+++ src/app/components/OrderDetail.tsx
@@ -7,9 +7,9 @@ function OrderDetail() {
     <section className="w-full mt-[30px] sm:mt-[45px]">
       <div className="px-[10px] flex justify-center flex-wrap gap-[15px] sm:px-[15px]">
         <SideBarMenu />
-        <div className="w-full border-[1.5px] border-double border-gray-300 md:w-[700px] rounded-md">
+        <div className="w-full border-[1.5px] border-double border-gray-300 xl:w-[700px] rounded-md">
           <div className="py-[30px]">
-            <div className="flex justify-between px-[15px]">
+            <div className="flex justify-between px-[15px] sm:px-[20px]">
               <div>
                 <Image
                   Src={"/assets/other/logo.png"}
@@ -29,7 +29,7 @@ function OrderDetail() {
             </div>
 
             <div className="mt-[30px]">
-              <div className="flex flex-col p-[15px] gap-[5px]">
+              <div className="flex flex-col py-[15px] px-[15px] sm:px-[20px] gap-[5px]">
                 <h2 className="text-[1.2rem] font-semibold uppercase">
                   Thông tin giao hàng:
                 </h2>
@@ -48,7 +48,7 @@ function OrderDetail() {
                 <table className="w-[200%] my-[15px] border-collapse text-[0.9rem] sm:w-full">
                   <thead>
                     <tr className="font-medium text-left bg-[#F1F2F4]">
-                      <th className="pl-[15px] py-[15px]">Tên</th>
+                      <th className="pl-[15px] sm:pl-[20px] py-[15px]">Tên</th>
                       <th className="p-[15px]">Giá</th>
                       <th className="p-[15px]">Số lượng</th>
                       <th className="p-[15px]">Thành tiền</th>
@@ -56,7 +56,7 @@ function OrderDetail() {
                   </thead>
                   <tbody>
                     <tr>
-                      <td className="pl-[15px] py-[15px]">
+                      <td className="pl-[15px] sm:pl-[20px] py-[15px]">
                         <div className="flex items-center gap-[10px]">
                           <Image
                             Src={"/assets/products/IMGSP3483.png"}
@@ -76,17 +76,21 @@ function OrderDetail() {
                     </tr>
 
                     <tr>
-                      <td className="p-[15px]">
+                      <td className="pl-[15px] sm:pl-[20px] p-[15px]">
                         <hr className="border border-black" />
                       </td>
                     </tr>
                     <tr className="text-[1rem]">
-                      <td className="font-semibold p-[15px]">Thành tiền:</td>
+                      <td className="font-semibold p-[15px] pl-[15px] sm:pl-[20px]">
+                        Thành tiền:
+                      </td>
                       <td className="p-[15px]">150,000₫</td>
                     </tr>
 
                     <tr className="text-[1rem]">
-                      <td className="font-semibold p-[15px]">Phí ship:</td>
+                      <td className="font-semibold p-[15px] pl-[15px] sm:pl-[20px]">
+                        Phí ship:
+                      </td>
                       <td className="p-[15px]">20,000₫</td>
                     </tr>
 
--- src/app/components/SideBarMenu.tsx
+++ src/app/components/SideBarMenu.tsx
@@ -8,55 +8,56 @@ import { HiOutlineUser } from "react-icons/hi";
 import { TbTruckDelivery } from "react-icons/tb";
 function SideBarMenu() {
   return (
-    <div className=" w-full lg:w-[320px]  p-[25px_20px] border-[1.5px] border-double border-gray-300 rounded-md">
-      
-      <Link href="/account">
-        <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
-          <div className="flex items-center gap-2.5 text-[0.95rem]">
-            <HiOutlineUser size={20} />
-            <span>Thông tin toàn khoản</span>
+    <div className=" w-full xl:max-w-[320px] border-[1.5px] border-double border-gray-300 rounded-md">
+      <div className="p-[25px_15px] sm:p-[30px_20px]">
+        <Link href="/account">
+          <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
+            <div className="flex items-center gap-2.5 text-[0.95rem]">
+              <HiOutlineUser size={20} />
+              <span>Thông tin toàn khoản</span>
+            </div>
+            <div>
+              <FiChevronRight size={18} />
+            </div>
           </div>
-          <div>
-            <FiChevronRight size={18} />
-          </div>
-        </div>
-      </Link>
+        </Link>
 
-      <Link href="/address">
-        <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
-          <div className="flex items-center gap-2.5 text-[0.95rem]">
-            <TbTruckDelivery size={22} />
-            <span>Địa chỉ giao hàng</span>
+        <Link href="/address">
+          <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
+            <div className="flex items-center gap-2.5 text-[0.95rem]">
+              <TbTruckDelivery size={22} />
+              <span>Địa chỉ giao hàng</span>
+            </div>
+            <div>
+              <FiChevronRight size={18} />
+            </div>
           </div>
-          <div>
-            <FiChevronRight size={18} />
-          </div>
-        </div>
-      </Link>
+        </Link>
 
-      <Link href="/order">
-        <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
-          <div className="flex items-center gap-2.5 text-[0.95rem]">
-            <IoBagHandleOutline size={20} />
-            <span>Đơn hàng của bạn</span>
+        <Link href="/order">
+          <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
+            <div className="flex items-center gap-2.5 text-[0.95rem]">
+              <IoBagHandleOutline size={20} />
+              <span>Đơn hàng của bạn</span>
+            </div>
+            <div>
+              <FiChevronRight size={18} />
+            </div>
           </div>
-          <div>
-            <FiChevronRight size={18} />
-          </div>
-        </div>
-      </Link>
+        </Link>
 
-      <Link href="/">
-        <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
-          <div className="flex items-center gap-2.5 text-[0.95rem]">
-            <LuDoorOpen size={22} />
-            <span>Đăng xuất</span>
+        <Link href="/">
+          <div className="flex justify-between items-center p-[13px] text-[15px] border border-gray-300 my-[12px] text-[#262626]">
+            <div className="flex items-center gap-2.5 text-[0.95rem]">
+              <LuDoorOpen size={22} />
+              <span>Đăng xuất</span>
+            </div>
+            <div>
+              <FiChevronRight size={18} />
+            </div>
           </div>
-          <div>
-            <FiChevronRight size={18} />
-          </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
   );
 }
