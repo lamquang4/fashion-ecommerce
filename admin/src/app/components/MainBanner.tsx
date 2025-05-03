5383523ab2db81e02d1ad69499b0b1c0b3a4188c
Quang Lam <lamdieuquang0105@gmail.com>
2025-05-04 02:16:07 +0700

    ewewew

--- admin/src/app/components/MainBanner.tsx
+++ admin/src/app/components/MainBanner.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import React from "react";
 import { VscTrash } from "react-icons/vsc";
-import { LiaEdit } from "react-icons/lia";
 import { IoMdAddCircle } from "react-icons/io";
 import { FaRegEyeSlash } from "react-icons/fa";
 import Pagination from "./Pagination";
