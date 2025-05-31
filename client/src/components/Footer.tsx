69862b1421a8888facb6fe46286ffad43bd6f9e9
Quang Lam <lamdieuquang0105@gmail.com>
2025-05-31 12:49:26 +0700

    ewewew

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,158 +5,167 @@ import { IoLogoYoutube } from "react-icons/io";
 function Footer() {
   return (
     <footer className="bg-gray-50 border-t border-gray-200 mt-[40px] sm:mt-[45px]">
-      <div className="border-b-2 border-gray-200 py-[80px]">
-        <div className="px-[10px] sm:px-[15px] flex justify-center flex-wrap gap-x-[20px] gap-y-[30px]">
-          <ul className="w-full lg:w-[calc(20%-16px)]">
-            <li>
-              <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
-                Chính sách
-              </h2>
-            </li>
+      <div className="mx-auto w-full max-w-screen-xl">
+        <div className="grid grid-cols-2 gap-8 px-[10px] sm:px-[15px] md:grid-cols-4 sm:py-[80px] py-[70px]">
+          <div>
+            <ul>
+              <li>
+                <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
+                  Chính sách
+                </h2>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
-          </ul>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-          <ul className="w-full lg:w-[calc(20%-16px)]">
-            <li>
-              <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
-                Chính sách
-              </h2>
-            </li>
+          <div>
+            <ul>
+              <li>
+                <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
+                  Chính sách
+                </h2>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
-          </ul>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-          <ul className="w-full lg:w-[calc(20%-16px)]">
-            <li>
-              <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
-                Chính sách
-              </h2>
-            </li>
+          <div>
+            <ul>
+              <li>
+                <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
+                  Chính sách
+                </h2>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
 
-            <li className="py-[8px]">
-              <Link
-                href="/"
-                className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-              >
-                Smtewewrwrwr
-              </Link>
-            </li>
-          </ul>
+              <li className="py-[8px]">
+                <Link
+                  href="/"
+                  className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                >
+                  Smtewewrwrwr
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-          <ul className="w-full lg:w-[calc(20%-16px)]">
-            <li>
-              <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
-                Theo dõi chúng tôi
-              </h2>
-            </li>
+          <div>
+            <ul>
+              <li>
+                <h2 className="relative font-bold text-black text-[0.935rem] uppercase mb-3 pb-1.5 after:content-[''] after:absolute after:bottom-0 after:left-0 after:bg-black after:w-[60px] after:h-[1.5px]">
+                  Theo dõi
+                </h2>
+              </li>
 
-            <li>
-              <ul className="flex justify-start items-center gap-3">
-                <li className="py-[8px]">
-                  <Link
-                    className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-                    href={"/"}
-                    title="Instagram"
-                  >
-                    <FaInstagram size={25} />
-                  </Link>
-                </li>
-                <li className="py-[8px]">
-                  <Link
-                    className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-                    href={"/"}
-                    title="Facebook"
-                  >
-                    <FaFacebookSquare size={25} />
-                  </Link>
-                </li>
-                <li className="py-[8px]">
-                  <Link
-                    className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
-                    href={"/"}
-                    title="Youtube"
-                  >
-                    <IoLogoYoutube size={25} />
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
+              <li>
+                <ul className="flex justify-start items-center gap-3">
+                  <li className="py-[8px]">
+                    <Link
+                      className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                      href={"/"}
+                      title="Instagram"
+                    >
+                      <FaInstagram size={25} />
+                    </Link>
+                  </li>
+                  <li className="py-[8px]">
+                    <Link
+                      className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                      href={"/"}
+                      title="Facebook"
+                    >
+                      <FaFacebookSquare size={25} />
+                    </Link>
+                  </li>
+                  <li className="py-[8px]">
+                    <Link
+                      className="inline-block max-w-max text-[#707072] font-medium text-[0.9rem]"
+                      href={"/"}
+                      title="Youtube"
+                    >
+                      <IoLogoYoutube size={25} />
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="text-center py-[15px] mx-auto">
-        <p className="text-[hsl(0,0%,47%)] text-[0.9375rem] font-medium capitalize">
-          © 2024 Aura - Lam Dieu Quang
-        </p>
+
+        <div className="text-center py-[15px] mx-auto">
+          <p className="text-[hsl(0,0%,47%)] text-[0.9375rem] font-medium capitalize">
+            © 2024 Aura - Lam Dieu Quang
+          </p>
+        </div>
       </div>
     </footer>
   );
