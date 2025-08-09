becbdad7b11ed9720023961e3d2cbfb1bc7aa168
Quang Lam <lamdieuquang0105@gmail.com>
2025-08-09 12:36:51 +0700

    rwr

--- admin/eslint.config.mjs
+++ admin/eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
     },
   },
 ];
