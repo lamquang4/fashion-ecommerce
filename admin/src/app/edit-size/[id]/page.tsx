9b803728e0f7f3bbf704c1430efb1359f2a94600
Quang Lam <lamdieuquang0105@gmail.com>
2025-08-09 14:09:51 +0700

    ewewew

--- admin/src/app/edit-size/[id]/page.tsx
+++ admin/src/app/edit-size/[id]/page.tsx
@@ -1,6 +1,5 @@
 import EditSize from "@/components/EditSize";
 import LayoutPage from "@/components/LayoutPage";
-import { Suspense } from "react";
 
 export default async function page() {
   return (
--- client/src/components/ProfileMenu.tsx
+++ client/src/components/ProfileMenu.tsx
@@ -15,34 +15,34 @@ function ProfileMenu({ isOpen }: ProfileMenuProp) {
     <div>
       {session?.user ? (
         <div className="w-[185px] absolute top-[22px] right-[-40px] overflow-hidden z-20 duration-400 ease-in-out bg-white shadow-md rounded-md ">
-          <h2 className="cursor-pointer border-b p-3 border-gray-300 text-[0.95rem] max-w-[210px] overflow-hidden text-ellipsis whitespace-nowrap text-center">
+          <h2 className="cursor-pointer border-b p-2.5 border-gray-300 text-[0.95rem] max-w-[210px] overflow-hidden text-ellipsis whitespace-nowrap text-center">
             Xin chào, {session?.user?.fullname}
           </h2>
 
           <Link
             href="/account"
-            className="hover:bg-[#F7F7F7] w-full block p-3 text-[0.95rem] text-black"
+            className="hover:bg-[#F7F7F7] w-full block p-2.5 text-[0.95rem] text-black"
           >
             Thông tin tài khoản
           </Link>
 
           <Link
             href="/address"
-            className="hover:bg-[#F7F7F7] w-full block p-3 text-[0.95rem] text-black"
+            className="hover:bg-[#F7F7F7] w-full block p-2.5 text-[0.95rem] text-black"
           >
             Sổ địa chỉ
           </Link>
 
           <Link
             href="/order"
-            className="hover:bg-[#F7F7F7] w-full block p-3 text-[0.95rem] text-black"
+            className="hover:bg-[#F7F7F7] w-full block p-2.5 text-[0.95rem] text-black"
           >
             Đơn hàng
           </Link>
 
           <button
             onClick={() => signOut()}
-            className="hover:bg-[#F7F7F7] w-full block p-3 text-[0.95rem] text-black text-left"
+            className="hover:bg-[#F7F7F7] w-full block p-2.5 text-[0.95rem] text-black text-left"
           >
             Đăng xuất
           </button>
@@ -51,14 +51,14 @@ function ProfileMenu({ isOpen }: ProfileMenuProp) {
         <div className="w-[120px] absolute top-[22px] right-[-40px] overflow-hidden z-20 duration-400 ease-in-out bg-white shadow-md rounded-md ">
           <Link
             href="/login"
-            className="hover:bg-[#F7F7F7] w-full block p-3 text-[0.95rem] text-black"
+            className="hover:bg-[#F7F7F7] w-full block p-2.5 text-[0.95rem] text-black"
           >
             Đăng nhập
           </Link>
 
           <Link
             href="/register"
-            className="hover:bg-[#F7F7F7] w-full block p-3 text-[0.95rem] text-black"
+            className="hover:bg-[#F7F7F7] w-full block p-2.5 text-[0.95rem] text-black"
           >
             Đăng kí
           </Link>
