cb2f45cd79effcaf8651dca51dcc7d6999fe35c7
Quang Lam <lamdieuquang0105@gmail.com>
2025-08-12 15:46:44 +0700

    ewewewew

---
+++ admin/src/app/api/get-notify-orders/route.ts
@@ -0,0 +1,26 @@
+import { connectMongoDB } from "@/lib/MongoConnect";
+import Order from "@/model/Order";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectMongoDB();
+    const limit = 10;
+    const startOfToday = new Date();
+    startOfToday.setHours(0, 0, 0, 0);
+
+    const orders = await Order.find({
+      createdAt: { $gte: startOfToday },
+    })
+      .sort({ createdAt: -1 })
+      .limit(limit);
+    return NextResponse.json({ orders });
+  } catch (err) {
+    return NextResponse.json(
+      { err, msg: "Lỗi" },
+      {
+        status: 400,
+      }
+    );
+  }
+}
---
+++ admin/src/hooks/useGetNotifyOrders.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import useSWR from "swr";
+import { Order } from "@/types/types";
+
+interface ResponseType {
+  orders: Order[];
+}
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+export default function useGetNotifyOrders() {
+
+  const url = `/api/get-notify-orders`;
+  const { data, error, isLoading, mutate } = useSWR<ResponseType>(url, fetcher);
+  return {
+    orders: data?.orders || [],
+    isLoading,
+    error,
+    mutate,
+  };
+}
