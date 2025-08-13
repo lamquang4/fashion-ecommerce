9e339e70839668ec97beb4f80ee7684e20e22245
Quang Lam <lamdieuquang0105@gmail.com>
2025-08-13 12:55:36 +0700

    ewew

--- admin/src/components/Notification.tsx
+++ admin/src/components/Notification.tsx
@@ -6,7 +6,7 @@ type menuProps = {
   toggleMenu: () => void;
 };
 function Notification({ menuOpen, toggleMenu }: menuProps) {
-  const { orders, isLoading } = useGetNotifyOrders();
+  const { orders } = useGetNotifyOrders();
   return (
     <div
       className="flex cursor-pointer items-center gap-[6px] text-[0.9rem] relative"
