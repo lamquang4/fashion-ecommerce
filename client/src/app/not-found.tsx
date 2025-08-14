4c3b7ab46f9536e3548812cbb383a33a3c0c902c
Quang Lam <lamdieuquang0105@gmail.com>
2025-08-14 22:00:14 +0700

    ewewe

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function NotFoundPage() {
   return (
     <>
-      <section className="px-[10px] mt-[40px] sm:mt-[45px] sm:px-[15px]">
+      <section className="px-[10px] my-[40px] sm:my-[45px] sm:px-[15px]">
         <div className="w-full mx-auto md:max-w-[1000px] lg:max-w-[1240px]">
           <div className="flex justify-center items-center h-[60vh]">
             <div>
@@ -12,16 +12,16 @@ function NotFoundPage() {
                 <Image
                   Src={"/assets/other/404-error.png"}
                   Alt={""}
-                  ClassName={"md:w-[380px] w-[300px]"}
+                  ClassName={"w-[250px]"}
                   loadingType="eager"
                 />
               </div>
 
               <div className="flex justify-center flex-col gap-3 items-center text-center">
-                <h2 className="text-[1.3rem] font-semibold">
+                <h2 className="text-[1.3rem] font-semibold uppercase">
                   TRANG KHÔNG ĐƯỢC TÌM THẤY
                 </h2>
-                <button className="text-[1rem] border border-black rounded-md font-medium p-[10px_15px] hover:bg-black hover:text-white">
+                <button className="text-[0.95rem] border border-black rounded-md font-medium px-3 py-2 hover:bg-black hover:text-white">
                   <Link href={"/"}>Về trang chủ</Link>
                 </button>
               </div>
