52666b4d351efdd2239b8399ddcd73612073f713
Quang Lam <lamdieuquang0105@gmail.com>
2025-09-12 19:53:55 +0700

    ewewe

--- client/src/app/blog/page.tsx
+++ client/src/app/blog/page.tsx
@@ -1,7 +1,12 @@
 import BlogAll from "@/components/BlogAll";
+import { Suspense } from "react";
 
 function page() {
-  return <BlogAll />;
+  return (
+    <Suspense>
+      <BlogAll />
+    </Suspense>
+  );
 }
 
 export default page;
