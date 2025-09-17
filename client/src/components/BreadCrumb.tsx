6d0969fbc62554819420bbea531cfce21139a25d
Quang Lam <lamdieuquang0105@gmail.com>
2025-09-17 08:24:50 +0700

    ewewe

--- client/src/components/BreadCrumb.tsx
+++ client/src/components/BreadCrumb.tsx
@@ -13,7 +13,7 @@ const BreadCrumb = ({ items }: Props) => {
     <div className="px-[10px] sm:px-[15px]">
       <div className=" w-full max-w-[1230px] mx-auto">
         <nav className="py-4">
-          <ol className="flex items-center gap-1 text-[0.9rem] text-gray-500 font-medium">
+          <ol className="flex items-center gap-1.5 text-[0.9rem] text-gray-500 font-medium">
             {items.map((item, index) => (
               <React.Fragment key={index}>
                 <li className="flex items-center gap-1">
@@ -53,16 +53,16 @@ const BreadCrumb = ({ items }: Props) => {
                   <li>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="lucide lucide-chevron-right last:hidden md:last:block"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      width="18"
+                      height="18"
+                      className="lucide lucide-chevron-right"
                       aria-hidden="true"
+                      viewBox="8 5 8 14"
                     >
                       <path d="m9 18 6-6-6-6"></path>
                     </svg>
