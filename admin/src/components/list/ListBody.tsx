08bb14201419f804c3f9031f3da1dead4286c980
Quang lam <lamdieuquang0105@gmail.com>
2026-02-09 01:01:51 +0700

    sửa màu nút

--- admin/src/components/list/ListBody.tsx
+++ admin/src/components/list/ListBody.tsx
@@ -10,3 +10,4 @@ function ListBody({ children }: Props) {
 }
 
 export default memo(ListBody);
+
--- admin/src/components/list/ListHeader.tsx
+++ admin/src/components/list/ListHeader.tsx
@@ -20,7 +20,7 @@ function ListHeader({ title, totalItems, addLink }: Props) {
         {addLink && (
           <Link
             href={addLink}
-            className="bg-[#E2EDFF] text-blue-500 hover:bg-blue-500 hover:text-white border-0 cursor-pointer text-[0.9rem] font-medium w-[90px] !flex p-[10px_12px] items-center justify-center gap-[5px]"
+            className="bg-[#daf4f0] text-[#0AB39C] hover:bg-[#0AB39C] hover:text-white border-0 cursor-pointer text-[0.9rem] font-medium w-[90px] !flex p-[10px_12px] items-center justify-center gap-[5px]"
           >
             <IoMdAddCircle size={22} /> Thêm
           </Link>
