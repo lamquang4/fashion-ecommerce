503752d11026a862c643af9cc679bec98d84920a
Quang lam <lamdieuquang0105@gmail.com>
2026-05-28 20:15:50 +0700

    fix: Update ProfileMenu

--- admin/src/components/layout/header/ProfileMenu.tsx
+++ admin/src/components/layout/header/ProfileMenu.tsx
@@ -18,45 +18,47 @@ function ProfileMenu({ menuOpen, onToggleMenu }: Props) {
     <>
       {session?.user && (
         <div
-          className=" text-[0.9rem] relative group"
+          className="relative"
           onMouseOver={onToggleMenu}
           onMouseOut={onToggleMenu}
         >
-          <div className="flex cursor-pointer items-center gap-[6px]">
-            <div className="w-[30px] rounded-full border border-gray-300 p-1">
+          <div className="flex cursor-pointer items-center gap-2">
+            <div className="w-[34px] h-[34px] p-1 rounded-full border border-gray-300 overflow-hidden">
               <Image
-                src={"/assets/other/owner.png"}
+                src={"/assets/other/user.png"}
                 alt={""}
-                className="w-full"
+                className="w-full h-full object-contain"
                 loading="eager"
               />
             </div>
-            <p>{session?.user.fullname || ""}</p>
+            <p className="font-medium">{session?.user.fullname || ""}</p>
           </div>
 
           {menuOpen && (
-            <div className="absolute top-full right-0 w-[185px] z-20 bg-white shadow-md rounded-md border border-gray-200">
-              <p className="border-b p-2.5 border-gray-300 max-w-[210px] overflow-hidden text-ellipsis whitespace-nowrap text-center">
+            <div
+              className={`absolute top-full right-0 max-w z-20 bg-white shadow-md rounded-md border border-gray-200`}
+            >
+              <p className="border-b p-3 border-gray-300 max-w-[210px] overflow-hidden text-ellipsis whitespace-nowrap text-center font-medium">
                 Xin chào, {session?.user?.fullname}
               </p>
 
               <Link
                 href={"/account"}
-                className="w-ful block hover:bg-gray-100 px-3 py-3.5"
+                className="block hover:bg-gray-100 p-3 whitespace-nowrap"
               >
-                <div className="flex items-center gap-[8px]">
+                <div className="flex items-center gap-2 font-medium">
                   <FaRegCircleUser size={18} />
                   <p>Tài khoản</p>
                 </div>
               </Link>
 
               <button
-                className="w-full block hover:bg-gray-100 px-3 py-3.5"
+                className="w-full hover:bg-gray-100 p-3 text-danger whitespace-nowrap"
                 onClick={() => {
                   signOut();
                 }}
               >
-                <div className="flex items-center gap-[8px] text-[#EF5F5F]">
+                <div className="flex items-center gap-2 font-medium text-[#EF5F5F]">
                   <RiLogoutBoxLine size={18} />
                   <p>Đăng xuất</p>
                 </div>
